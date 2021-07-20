9d428dd918330509305db36e4e9cff9b6c46d680
Daniel Tabellion <wellos94@web.de>
2021-07-20 11:39:59 +0200

    deleted everything but vim plugin

--- codebase2/codebase/U/Codebase/WatchKind.hs
+++
@@ -1,3 +0,0 @@
-module U.Codebase.WatchKind where
-
-data WatchKind = RegularWatch | TestWatch deriving (Eq, Ord, Show)
--- scripts/unisonloop.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-until (stack build && \
-       stack exec unison)
-do
-  echo "Well that didn't work."
-  echo Press any key to re-launch.
-  read -n 1
-done
